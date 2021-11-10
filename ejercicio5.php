eea2b2b5f3b5aed9ee219e55fdf1d03383dcd3c5
alvarogamez28 <alvarogamjim2ciclo@gmail.com>
2021-11-10 12:34:35 +0100

    ejer

---
+++ ejercicio5.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include "ejercicio5BBDD.php";
+   
+    if ($_SERVER["REQUEST_METHOD"]=="POST") {
+        $nombre = $_POST["nombre"];            
+        $contraseña = $_POST["contraseña"];            
+        $cuentaBancaria = $_POST["cuentaBancaria"];       
+        $contraseña = password_hash("contraseña",PASSWORD_DEFAULT);
+        insertaElemento($nombre,$contraseña,$cuentaBancaria);
+    }
+    ?>
+
+<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+  <p>Nombre: <input type="text" name="nombre" placeholder="Nombre"></p>
+  <p>Contraseña: <input type="password" name="contraseña" placeholder="contraseña"></p>
+  <p>Cuenta Bancaria: <input type="number" name="cuentaBancaria" placeholder="cuenta"></p>
+  <p>Registrarse: <input type="submit" name="registro" placeholder="registro"></p>
+  
+</form>
+</body>
+</html>
---
+++ ejercicio5BBDD.php
@@ -0,0 +1,24 @@
+<?php
+$servidor = "localhost";
+$basesDatos = "usuarios";
+$usuario = "root";
+$pass = "root";
+
+$coca = "mucha coca";
+
+function insertaElemento($nombre,$contraseña,$cuentaBancaria){
+    try {
+        $conexion = new PDO("mysql:host=$GLOBALS[servidor];dbname=$GLOBALS[basesDatos]",$GLOBALS['usuario'],$GLOBALS['pass']);
+        $consulta = $conexion->prepare("INSERT INTO usuarios (nombre,contraseña,cuentaBancaria) VALUES (?,?,?)");
+        $consulta->bindParam(1, $nombre);
+        $consulta->bindParam(2, $contraseña);
+        $consulta->bindParam(3, $cuentaBancaria);
+        $consulta->execute();
+        return $conexion->lastInsertId();
+        $conexion = null;
+    } catch (PDOException $e) {
+        echo "Conexión fallida: " . $e->getMessage();
+    }
+   }
+
+?>
