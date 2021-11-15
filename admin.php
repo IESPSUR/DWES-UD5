e2541933a8b2678f9e9966dc8f144b948d0ca572
alvarogamez28 <alvarogamjim2ciclo@gmail.com>
2021-11-15 12:13:11 +0100

    ejer

--- admin.php
+++ admin.php
@@ -1,13 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-    session_start();
-    if ($_SESSION['perfil']=="") {
-        header('Location: ejercicio6.php');
-    }
-    elseif ($_SESSION['perfil']=="usuario") {
-        header('Location: usuario.php');
-    }
-    else  {
-        echo "Eres el jefe supremo";
-    }
+        session_start();
+        if ($_SESSION['perfil']=="admin") {
+            echo "Eres administrador";
+        }
+        else if ($_SESSION['perfil']=="usuario") {
+            header('Location: usuario.php');
+        }
+        else  {
+            header('Location: ejercicio7.php');
+        }    
 
 ?>
+<a href='cierraSesion.php'>Cerrar Sesion</a>
+</body>
+</html>
--- usuario.php
+++ usuario.php
@@ -1,13 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-    session_start();
-    if ($_SESSION['perfil']=="") {
-        header('Location: ejercicio6.php');
-    }
-    elseif ($_SESSION['perfil']=="admin") {
-        header('Location: admin.php');
-    }
-    else  {
-        echo "Hola pringao";
-    }
+        session_start();
+        if ($_SESSION['perfil']=="usuario") {
+            echo "Eres usuario";
+        }
+        else if ($_SESSION['perfil']=="admin") {
+            header('Location: admin.php');
+        }
+        else  {
+            header('Location: ejercicio7.php');
+        }    
 
 ?>
+<a href='cierraSesion.php'>Cerrar Sesion</a>
+</body>
+</html>
